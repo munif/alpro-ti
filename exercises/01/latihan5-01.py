485e1ea7aaf18b82b7d77d8af5e7cde6a3f218fa
Abdul Munif <abdulmuneverlose@gmail.com>
2017-03-14 13:13:13 +0700

    Adding exercises

---
+++ exercises/01/latihan5-01.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 14 12:05:25 2017
+
+@author: Abdul Munif
+"""
+
+suhu_celcius = input('Masukkan suhu dalam skala Celcius: ')
+suhu_reamur = 4.0/5.0 * suhu_celcius
+
+print("Suhu Celcius: %f dikonversi ke dalam Reamur menjadi: %f" %(suhu_celcius, suhu_reamur))
